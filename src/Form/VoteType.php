1c51c479db78b3cdbe981a535b43c45efa9ee45d
Toam25 <toarymanana@gmail.com>
2021-01-07 20:23:09 +0300

    Update voteType

---
+++ src/Form/VoteType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Vote;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('photos')
+            ->add('description',TextareaType::class,[
+                'attr'=>[
+                    'class'=>'form-control',
+                    'id'=>'kmlk'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Vote::class,
+        ]);
+    }
+}
