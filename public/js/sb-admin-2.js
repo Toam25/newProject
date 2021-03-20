60521d07b85496bfa157f1ce7eb9de669079e2c0
Toam25 <toarymanana@gmail.com>
2021-03-20 19:10:44 +0100

    delate js

--- public/js/sb-admin-2.js
+++ public/js/sb-admin-2.js
@@ -35,14 +35,14 @@
   });
 
   // Scroll to top button appear
-  $(document).on('scroll', function() {
+  /*$(document).on('scroll', function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
     }
-  });
+  });*/
 
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(e) {
