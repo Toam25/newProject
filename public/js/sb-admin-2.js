25c48f95a16e9a928bd866db50fd0d298ade9867
Toam25 <toarymanana@gmail.com>
2021-03-20 19:15:17 +0100

    fix sidenav autimatcly close

--- public/js/sb-admin-2.js
+++ public/js/sb-admin-2.js
@@ -25,24 +25,24 @@
   });
 */
   // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  /*$('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
     if ($(window).width() > 768) {
       var e0 = e.originalEvent,
         delta = e0.wheelDelta || -e0.detail;
       this.scrollTop += (delta < 0 ? 1 : -1) * 30;
       e.preventDefault();
     }
-  });*/
+  });
 
   // Scroll to top button appear
-  /*$(document).on('scroll', function() {
+  $(document).on('scroll', function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
     }
-  });*/
+  });
 
   // Smooth scrolling using jQuery easing
   $(document).on('click', 'a.scroll-to-top', function(e) {
