646c06a19417760bee48f7ad5e54e09b4070a3c8
Toam25 <toarymanana@gmail.com>
2021-03-22 04:31:34 +0300

    update controller

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -61,35 +61,17 @@ class BoutiqueController extends AbstractController
     /**
      * @Route("/shop/{type}/{id}", name="shop", defaults = {"id"=null})
      */
-    public function boutique($type = null, $id, Request $request, BoutiqueRepository $boutiqueRepository, SearchService $searchService, ArticleRepository $articleRepository)
+    public function boutique($type = "", int $id, Request $request, BoutiqueRepository $boutiqueRepository, SearchService $searchService, ArticleRepository $articleRepository)
     {
-
-
-        $listShops = $this->getlistShop($boutiqueRepository->findBy(['type' => $type]), $type);
 
         $pageWasRefreshed = isset($_SERVER['HTTP_CACHE_CONTROL']) && $_SERVER['HTTP_CACHE_CONTROL'] === 'max-age=0';
 
-        if ($id == -1) {
-            $boutique = $boutiqueRepository->findOneBy(['type' => $type]);
-        } else {
+        $listShops = $this->getlistShop($boutiqueRepository->findBy(['type' => $type]), $type);
 
-            if ($pageWasRefreshed) {
-                $shopId = $id;
-            } else {
-                $shopId = $id; //$this->lastShopVisited($listShops['allShopId']);
-            }
-
-            $boutique = $boutiqueRepository->findOneByWithHeaderReference($type,intval($shopId));
-        }
-
-        if ($request->get('shop_id')) {
-            $article = $searchService->getResultSearch($request);
-        } else {
-            $article = $articleRepository->findAllArticleByBoutique($boutique);
-        }
-
-
-
+        $boutique = $type!="" ? $boutiqueRepository->findOneByWithHeaderReference($type,intval($id)) : 'super_admin';
+        
+        $article = $request->get('shop_id') ? $article = $searchService->getResultSearch($request) : $articleRepository->findAllArticleByBoutique($boutique);
+     
         return $this->render('boutique/boutique.html.twig', [
             'controller_name' => 'BoutiqueController',
             'boutique' => $boutique,
