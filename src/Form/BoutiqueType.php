f08653aae13d5bcb074e0c556796f81069219323
Toam25 <toarymanana@gmail.com>
2021-03-22 11:51:17 +0100

    edit form

--- src/Form/BoutiqueType.php
+++ src/Form/BoutiqueType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
