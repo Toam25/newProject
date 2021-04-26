37f937c95fea640e37aa6685f8baf3fcd02efd9a
Toam25 <toarymanana@gmail.com>
2021-04-26 12:02:21 +0300

    change wikimedia leflet to openstreet map

--- public/js/config-admin-leaflet.js
+++ public/js/config-admin-leaflet.js
@@ -12,8 +12,8 @@ $(function () {
         map.setView([51.505, -0.09], 13);
     }
 
-    L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'/*'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'*/, {
-        attribution: 'cMap data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'/*'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'/*'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}'*/, {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'/*cMap data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'*/,
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         tileSize: 512,
