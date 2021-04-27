2ef7a58edadf6ec65d7879bd8b877cc994fddac8
Toam25 <toarymanana@gmail.com>
2021-04-27 14:26:38 +0300

    remplace findByAllValidateBlog by findByNotValidateBlog

--- src/Controller/BoutiqueController.php
+++ src/Controller/BoutiqueController.php
@@ -113,7 +113,7 @@ class BoutiqueController extends AbstractController
         } else {
 
             $article = $articleRepository->findAllArticleByBoutique($boutique);
-            $blogs = $blogRepository->findAllBlogByBoutique($boutique);
+            $blogs = $blogRepository->findByNotValidateBlog($boutique);
         }
 
         if ($boutique) {
